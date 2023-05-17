aad420857047f99089b302bb469d2540d345f4a2
ricky-kiva <ricky.kiva@gmail.com>
2023-05-18 01:01:33 +0700

    add Banner Component

--- app/src/main/java/com/rickyslash/composecafe/MainActivity.kt
+++ app/src/main/java/com/rickyslash/composecafe/MainActivity.kt
@@ -3,13 +3,17 @@ package com.rickyslash.composecafe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rickyslash.composecafe.components.SearchBar
 import com.rickyslash.composecafe.ui.theme.ComposeCafeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +27,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComposeCafe() {
+    Column {
+        Banner()
+    }
+}
 
+@Composable
+fun Banner(modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp))
+        SearchBar()
+    }
 }
 
 @Preview(showBackground = true)
