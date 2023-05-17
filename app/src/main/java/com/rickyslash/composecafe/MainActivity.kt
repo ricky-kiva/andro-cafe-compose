c9e8dabe750c1c7f455a56478d8cc950ace46b9c
ricky-kiva <ricky.kiva@gmail.com>
2023-05-17 23:51:00 +0700

    Modifier summary

--- app/src/main/java/com/rickyslash/composecafe/MainActivity.kt
+++ app/src/main/java/com/rickyslash/composecafe/MainActivity.kt
@@ -81,3 +81,69 @@ Row {
     ButtonWithText("2", Modifier.weight(2f))
     ButtonWithText("3 fill true", Modifier.weight(weight = 3f, fill = true))
 }*/
+
+// Modifier: parameter to modify existing Composable Function, including:
+// - size, range, display, position
+// - action on certain condition
+// - add action to some component
+/* example:
+Row(
+    verticalAlignment = Alignment.CenterVertically
+    modifier = modifier
+        .fillMaxWidth()
+        .clickable(onClick = {})
+        .padding(4.dp),
+)*/
+
+// Size: set layout size in Compose using Modifier
+// - fillMaxSize: component fill parent's layout
+// - wrapContentSize: wrap the content inside
+// - size: use exact value
+// - requiredSize: use exact value, not depends on parent's layout
+// - sizeIn: use range for size
+// - can also use `width` & `height`
+
+// Action: modifier to add action on any component
+// - Clickable: add click action
+// - Draggable: add drag action
+// - Selectable: make component can be selected
+// - Swipeable: add swipe action
+/* example:
+Row(
+    modifier = Modifier
+        .fillMaxWidth()
+        .clickable(onClick = {}
+)*/
+
+// Padding, Offset, & Spacer:
+// - Padding:
+// --- all
+// --- start, top, bottom, end
+// --- horizontal, vertical
+// - Offset: `Modifier.offset(x=8.dp, y = 30.dp)`
+// - Spacer: same like padding but can't be clicked
+
+// Drawing: add accessory to component:
+// - border, clip, alpha, background, shadow
+
+// in writing Modifier, order of writing is important:
+/* this example made the padding area NOT clickable:
+Row(
+    modifier = Modifier
+        .fillMaxWidth()
+        .clickable(onClick = {})
+        .padding(12.dp),
+…*/
+
+// Modifier as Parameter: best practice is to give default value to Modifier
+/* example:
+@Composable
+fun ButtonWithText(text: String, modifier: Modifier = Modifier) {
+    Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(colorResource(R.color.purple_500)),
+        modifier = modifier.padding(4.dp)
+    ) {
+        Text(text, textAlign = TextAlign.Center)
+    }
+}*/
